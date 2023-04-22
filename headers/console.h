5979987ad4167d8c174254051558b7c78a40a4a1
Junior <abelsouzacosta@gmail.com>
2023-04-22 20:44:43 -0300

    [chore]: headers
    adding header for console operations

---
+++ headers/console.h
@@ -0,0 +1,10 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+void write_console(char *str, ...); // color set magenta
+
+void throw_error(char *str, ...); // color set red
+
+void callout(char *str, ...); // color set yellow
+
+#endif
