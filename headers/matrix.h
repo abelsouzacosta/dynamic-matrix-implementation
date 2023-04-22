b1cb0cba49b6caabe03cb5452476d6f029f69514
Junior <abelsouzacosta@gmail.com>
2023-04-22 20:40:38 -0300

    [chore]: matrix
    adding header file for matrix operations

---
+++ headers/matrix.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+int **create_matrix(int rows, int columns);
+
+void fill_matrix(int **matrix, int rows, int columns);
+
+void print_matrix(int **matrix, int rows, int columns);
+
+#endif
